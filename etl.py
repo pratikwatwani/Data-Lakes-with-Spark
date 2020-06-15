95c150a242f9955a9bc8ca17b321c93034ede4e0
Pratik Watwani <pratikpwatwani@gmail.com>
2020-06-15 08:47:45 -0400

    updated etl and sh file

--- etl.py
+++ etl.py
@@ -101,7 +101,7 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3://spark-dataLakes/"
+    input_data = "s3://udacity-dend/"
     output_data = "s3://spark-dataLakes/output/"
 
     process_song_data(spark, input_data, output_data)
---
+++ run.sh
@@ -0,0 +1,1 @@
+spark-submit etl.py --master yarn --deploy-mode client --driver-memory 5g --num-executors 3 --executor-memory 3G --executor-core 2
